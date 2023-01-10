bfe2212aefc696a39bea792f9601577b7d5ad54f
Ectimel <dtworek94@gmail.com>
2023-01-10 04:08:20 +0000

    created simple table component with row component inside

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoTable from "./components/TodoTable";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const todos = [
+        {rowNumber: 1, rowDescription: `Be Awesome`, rowAssigned: `User One`},
+        {rowNumber: 2, rowDescription: `Be Great Person`, rowAssigned: `User Two`},
+        {rowNumber: 3, rowDescription: `Love Your self`, rowAssigned: `User Three`},
+        {rowNumber: 4, rowDescription: `Do something amazing`, rowAssigned: `User Three`}
+
+    ];
+
+
+    return (
+        <div className={`mt-5 container`}>
+            <div className={`card`}>
+                <div className={`card-header`}>
+                    Your Todo's
+                </div>
+                <div className={`card-body`}>
+                    <TodoTable todos={todos}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
