9f7e7239c12927ff7043034658e151e0c99bb884
Ectimel <dtworek94@gmail.com>
2023-01-11 04:42:30 +0000

    added React state to re-render our application after we click add button. for now added data is hardcoded but that will change in close future

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 import TodoTable from "./components/TodoTable";
 
 function App() {
-    const todos = [
+    const [todos, setTodos] = useState([
         {rowNumber: 1, rowDescription: `Be Awesome`, rowAssigned: `User One`},
         {rowNumber: 2, rowDescription: `Be Great Person`, rowAssigned: `User Two`},
         {rowNumber: 3, rowDescription: `Love Your self`, rowAssigned: `User Three`},
         {rowNumber: 4, rowDescription: `Do something amazing`, rowAssigned: `User Three`}
+    ]);
 
-    ];
-
+    const add = () => {
+        if(todos.length > 0) {
+            const newTodos = {
+                rowNumber: todos.length,
+                rowDescription: `What's up!`,
+                rowAssigned: `Awesome User`
+            };
+            setTodos(todos => [...todos, newTodos]);
+            console.log(newTodos);
+        }
+    }
 
     return (
         <div className={`mt-5 container`}>
@@ -20,6 +30,7 @@ function App() {
                 </div>
                 <div className={`card-body`}>
                     <TodoTable todos={todos}/>
+                    <div className="btn btn-primary" onClick={add}>Add</div>
                 </div>
             </div>
         </div>
